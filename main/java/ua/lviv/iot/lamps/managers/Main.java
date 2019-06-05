01bf5773b3179cfba900d04d007188926c070f34
VovkIlona <47509136+VovkIlona@users.noreply.github.com>
2019-06-05 18:02:23 +0300

    9-lab
    9-lab

---
+++ main/java/ua/lviv/iot/lamps/managers/Main.java
@@ -0,0 +1,17 @@
+package ua.lviv.iot.lamps.managers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ua.lviv.iot.lamps.models.Currency;
+import ua.lviv.iot.lamps.models.Lamps;
+import ua.lviv.iot.lamps.models.LightSourceType;
+import ua.lviv.iot.lamps.models.Rating;
+
+public class Main {
+  public static void main(String[] args) {
+
+  
+  }
+
+}
